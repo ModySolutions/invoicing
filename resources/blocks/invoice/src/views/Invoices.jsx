e81ea9d988f6a72319e1921641f732620c5b49f8
Javier Troya <javiertroya@gmail.com>
2025-03-07 16:37:16 +0100

    Save draft. Status filter dropdown.

--- resources/blocks/invoice/src/views/Invoices.jsx
+++ resources/blocks/invoice/src/views/Invoices.jsx
@@ -1,14 +1,34 @@
-import {useState} from "react";
-import InvoiceListStatusButtons from "../components/invoice-list/InvoiceListStatusButtons";
+import {useEffect, useState} from "react";
+import {__} from '@wordpress/i18n';
 import InvoicesList from "../components/InvoicesList";
+import InvoiceStatusDropdown from "../components/invoice-common/InvoiceStatusDropdown";
+import {useInvoices} from "../contexts/InvoicesContext";
+import Enums from "../../../../scripts/tools/Enums";
 
 const Invoices = () => {
+    const {currentStatus, setCurrentStatus, setCurrentStatusLabel, setFetchNewInvoices} = useInvoices();
     const [statuses] = useState(Invoice.statuses);
+
+    useEffect(() => {}, [currentStatus])
 
     return (
         <>
             <div>
-                <InvoiceListStatusButtons statuses={statuses}/>
+                <div className='flex flex-row-reverse gap-x-2'>
+                    <InvoiceStatusDropdown
+                        className='w-200'
+                        status={currentStatus}
+                        onClick={(status, label) => {
+                            setCurrentStatus(status);
+                            setCurrentStatusLabel(label)
+                            setFetchNewInvoices(true);
+                        }}
+                        params={{showAll: true, allValue: 'any', allLabel: Enums.STATUS.LABELS['any']}}
+                    />
+                    <div className='w-400'>
+                        {/*<input placeholder={__('Search...', 'app')} />*/}
+                    </div>
+                </div>
                 <InvoicesList statuses={statuses} />
             </div>
 
